676e42bff378f5804f6c0e44d233ef0037c5622e
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2014-05-28 15:21:10 -0700

    first commit

--- problem_3/p3.hs
+++ problem_3/p3.hs
@@ -1,6 +1,6 @@
 largestFactor' :: (Integral a) => a -> a -> a -> [a] -> [a]
 largestFactor' n cur orig facs = 
-  if (cur*cur > orig) then facs
+  if (cur > orig) then facs
   else 
     if mod n cur == 0 then largestFactor' (div n cur) cur orig $ facs ++ [cur]
     else largestFactor' n (cur+1) orig facs
